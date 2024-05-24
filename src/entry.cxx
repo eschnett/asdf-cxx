6d5ed2f3758d285ed8c392f927107f1ca0a9d8e4
Erik Schnetter <schnetter@gmail.com>
2024-05-24 09:48:36 -0400

    Avoid some compiler warnings

--- src/entry.cxx
+++ src/entry.cxx
@@ -3,6 +3,7 @@
 #include <asdf/datatype.hxx>
 
 #include <cctype>
+#include <cstdlib>
 #include <optional>
 #include <sstream>
 
@@ -212,6 +213,7 @@ std::shared_ptr<entry> make_entry(const std::shared_ptr<reader_state> &rs,
   }
 
   assert(0);
+  std::abort();
 }
 
 } // namespace ASDF
